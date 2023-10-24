3f86d23784d1f0516b631a85cf261bfacef0e003
Aditya01jain <118585917+Aditya01jain@users.noreply.github.com>
2023-10-24 18:24:58 +0530

    changes
    changes

--- calculator/src/components/Buttons.js
+++
@@ -1,8 +0,0 @@
-import './Buttons';
-function Buttons(props) {
-    return(
-        <input type='button' value={props.label} ></input>
-    );
-
-}
-export default Buttons;
--- calculator/src/components/Output.js
+++
@@ -1,9 +0,0 @@
-import './Output';
-function Output(props) {
-    return(
-        <div className='output-screen'>
-            <input type='text' readOnly value={props.value}>{0}</input>
-        </div>
-    );
-}
-export default Output;
